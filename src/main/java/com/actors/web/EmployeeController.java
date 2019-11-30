a58b7e5d869491163a5d9f3c8c6c6af0ac3bc136
Ajinkya <ajinkya@symboinsurance.com>
2019-11-30 11:00:27 +0530

    Basic project setup

--- src/main/java/com/actors/web/EmployeeController.java
+++ src/main/java/com/actors/web/EmployeeController.java
@@ -3,11 +3,13 @@ package com.actors.web;
 import com.actors.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ActorController {
+@RequestMapping("/employee")
+public class EmployeeController {
 
     @Autowired
     private EmployeeService employeeService;
